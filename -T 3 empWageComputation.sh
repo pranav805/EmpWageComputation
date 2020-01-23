772a20f69478ebd2cc08c170f309aa36310bfdf4
pranav805 <pranav.ige96@gmail.com>
2020-01-23 17:57:22 +0530

    MODIFIED: changed to while condition

--- -T 3 empWageComputation.sh
+++ -T 3 empWageComputation.sh
@@ -1,16 +1,23 @@
 #!/bin/bash -x
-
-echo "welcome to Employee Wage Computation"
-EMP_WORKING_PARTIME=1
-EMP_WORKING_FULLTIME=2
+echo "Welcome to Employee Wage Computation"
+EMPLOYEE_WORKING_PARTTIME=1
+EMPLOYEE_WORKING_FULLTIME=2
 WAGE_PER_HOUR=20
-RandomCheck=$((RANDOM%3))
-if [ $RandomCheck -eq $EMP_WORKING_PARTIME ]
-then
-	empHrs=4
-	fullDayHour=8
-	DailyWage=$(($WAGE_PER_HOUR * $fullDayHour))
-else
-	echo "Employee is Absent"
-	DailyWage=0
-fi
+WORKING_DAYS_PER_MONTH=20
+for (( day=1; day<=$WORKING_DAYS_PER_MONTH; day++ ))
+do
+	RandomCheck=$((RANDOM%3))
+	case $RandomCheck in
+		$EMPLOYEE_WORKING_PARTTIME)
+		empHrs=4
+		;;
+		$EMPLOYEE_WORKING_FULLTIME)
+		empHrs=8
+		;;
+		*)
+		empHrs=0
+		;;
+	esac
+EmployeeWage=$(($WAGE_PER_HOUR*$empHrs))
+TotalSalery=$(($TotalSalery+$EmployeeWage))
+done
--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,11 +1,18 @@
 #!/bin/bash -x
 echo "Welcome to Employee Wage Computation"
+#Constants
 EMPLOYEE_WORKING_PARTTIME=1
 EMPLOYEE_WORKING_FULLTIME=2
 WAGE_PER_HOUR=20
 WORKING_DAYS_PER_MONTH=20
-for (( day=1; day<=$WORKING_DAYS_PER_MONTH; day++ ))
+MAX_WORKING_HOURS_OF_EMPLOYEE=100
+
+#Variables
+totalEmployeeHrs=0
+totalEmployeeDays=0
+while [[ $totalEmployeeHrs -lt $MAX_WORKING_HOURS_OF_EMPLOYEE &&  $totalEmployeeDays -lt $WORKING_DAYS_PER_MONTH ]]
 do
+	((totalEmployeeDays++))
 	RandomCheck=$((RANDOM%3))
 	case $RandomCheck in
 		$EMPLOYEE_WORKING_PARTTIME)
@@ -18,7 +25,8 @@ do
 		empHrs=0
 		;;
 	esac
-EmployeeWage=$(($WAGE_PER_HOUR*$empHrs))
-TotalSalery=$(($TotalSalery+$EmployeeWage))
+totalEmployeeHrs=$(($totalEmployeeHrs + $empHrs))
 done
+TotalSalery=$(($totalEmployeeHrs*$WAGE_PER_HOUR))
+
 
