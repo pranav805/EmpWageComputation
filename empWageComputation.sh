11f94c381959c768bab96a51ff1beba80f155bbc
pranav805 <pranav.ige96@gmail.com>
2020-01-23 15:14:52 +0530

    ADDED: Case Statements

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -4,14 +4,15 @@ EMPLOYEE_WORKING_PARTTIME=1
 EMPLOYEE_WORKING_FULLTIME=2
 WAGE_PER_HOUR=20
 RandomCheck=$((RANDOM%3))
-if [ $RandomCheck -eq $EMPLOYEE_WORKING_PARTTIME ]
-then
-	empHrs=4
-elif [ $RandomCheck -eq $EMPLOYEE_WORKING_FULLTIME ]
-then 
-
-	empHrs=8
-else
-	empHrs=0
-fi
+case $RandomCheck in
+	$EMPLOYEE_WORKING_PARTTIME)
+		empHrs=4
+		;;
+	$EMPLOYEE_WORKING_FULLTIME)
+		empHrs=8
+		;;
+	*)
+		empHrs=0;;
+esac
 EmpWage=$(($WAGE_PER_HOUR * $empHrs))
+
