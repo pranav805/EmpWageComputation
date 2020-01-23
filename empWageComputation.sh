d2aec0d0a0cb06343207c2d6deaa5a27965225d3
pranav805 <pranav.ige96@gmail.com>
2020-01-23 13:10:30 +0530

    ADDED: Daily Employee Wage

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -2,10 +2,14 @@
 
 echo "welcome to Employee Wage Computation"
 isPresent=1
+WAGE_PER_HOUR=20
 Random=$((RANDOM%2))
 if [ $isPresent -eq $Random ]
 then
 	echo "Employee is Present"
+	fullDayHour=8
+	DailyWage=$(($WAGE_PER_HOUR * $fullDayHour))
 else
 	echo "Employee is Absent"
+	DailyWage=0
 fi
