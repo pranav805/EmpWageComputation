b5c2d4779918f6a43cb887653091a9298f9d2b0c
pranav805 <pranav.ige96@gmail.com>
2020-01-27 10:12:46 +0530

    REFACTORED: Changed indentations

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -23,8 +23,7 @@ function getWorkHours()
       *)
       empHrs=0
       ;;
-
- 	esac
+	esac
 echo $empHrs
 }
 
@@ -34,6 +33,7 @@ function dailyWage()
 	wage=$(($empHrs*$WAGE_PER_HOUR))
 	echo $wage
 }
+
 while [[ $totalEmployeeHrs -lt $MAX_WORKING_HOURS_OF_EMPLOYEE &&  $totalEmployeeDays -lt $WORKING_DAYS_PER_MONTH ]]
 do
 	((totalEmployeeDays++))
@@ -41,6 +41,7 @@ do
 	totalEmployeeHrs=$(($totalEmployeeHrs + $empHrs))
 	empDailyWage[$totalEmployeeDays]="$( dailyWage $empHrs )"
 done
+
 TotalSalery="$( dailyWage $totalEmployeeHrs )"
 echo "Daily Wage: " ${empDailyWage[@]}
 echo "Day: " ${!empDailyWage[@]}
